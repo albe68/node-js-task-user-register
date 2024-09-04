2c7f9a6804e64ee6b5383d6297851dd6aa23edbe
Albert Remalin <albertpaulus836@gmail.com>
2024-09-04 12:41:41 +0530

    chore: cleared log lines

--- server/config.js
+++ server/config.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 
 dotenv.config({});
-
+console.log(process.env.NODE_ENV);
 export const envConfig = {
   PORT: process.env.PORT || 8888,
 
   MONGODB_URI: process.env.MONGODB_CONNECTION_URI || "mongodb://0.0.0.0:27017/register_app",
 
   ENVIRONMENT: process.env.ENV || "development",
+
+  
 };
